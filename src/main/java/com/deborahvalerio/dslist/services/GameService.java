f810b9dcfb5e8cb1be38fcbe34555b6dff0ff4bf
DeborahValerio <dborah.v@gmail.com>
2023-05-10 15:49:34 -0300

    Find game by id

--- src/main/java/com/deborahvalerio/dslist/services/GameService.java
+++ src/main/java/com/deborahvalerio/dslist/services/GameService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.deborahvalerio.dslist.dto.GameDTO;
 import com.deborahvalerio.dslist.dto.GameMinDTO;
 import com.deborahvalerio.dslist.entities.Game;
 import com.deborahvalerio.dslist.repositories.GameRepository;
@@ -15,9 +18,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x-> new GameMinDTO(x)).toList();
 	}
+	
 
 }
